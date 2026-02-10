478a5e2557ebd833bc84a62e219b8830376b7ba8
Sailee Das <sailee.das123@gmail.com>
2026-02-09 20:59:01 -0800

    largest number

---
+++ LargestIntegerProblem.java
@@ -0,0 +1,26 @@
+package com.test;
+
+public class LargestIntegerProblem {
+
+	public static void main(String[] args) 
+	{
+		int nums[]= {3, 3, 0, 99, -40};
+		largestElement(nums);
+	}
+		    private static int largestElement(int[] nums) {
+		        int largest=nums[0];
+		      for(int i=0;i<nums.length;i++)
+		      {
+		        
+		       if(nums[i]>largest)
+		       {
+		         largest=nums[i];
+		       }
+		      }
+		      return largest;
+		    }
+		
+
+	
+
+}
